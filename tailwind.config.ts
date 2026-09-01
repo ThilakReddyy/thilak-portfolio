98211146cbf188f5839ddee693ab08a5fa67431e
thilakreddy <thilakreddy19102000@gmail.com>
2026-09-01 15:44:02 +0530

    change: flow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
   darkMode: ["class"],
@@ -85,5 +84,5 @@ export default {
       },
     },
   },
-  plugins: [tailwindcssAnimate],
+  plugins: [],
 } satisfies Config;
